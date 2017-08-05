637b7608658ed982ef6fa3bdd09c3c1d20a59f72
mknieszner <mknieszner@gmail.com>
2017-08-06 00:13:30 +0200

    build modification

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,8 @@
 package com.kodilla.testing;
 
+/**
+ * This class do nothing :D
+ */
 public class TestingMain {
     public static void main(String[] args){
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
