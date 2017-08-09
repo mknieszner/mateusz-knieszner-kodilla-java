113ce6a3fb4fcd6c40157fd79486b321febc35d3
mknieszner <mknieszner@gmail.com>
2017-08-09 20:26:32 +0200

    7.1

---
+++ kodilla-stream/src/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.decorate("Give ", stringToDecorate ->
+                System.out.print(stringToDecorate.toLowerCase()));
+        poemBeautifier.decorate("me", stringToDecorate ->
+                System.out.print(" " + stringToDecorate + " "));
+        poemBeautifier.decorate(" a ", stringToDecorate ->
+                System.out.print(stringToDecorate.trim()));
+        poemBeautifier.decorate("challenge!", stringToDecorate ->
+                System.out.println(" "+stringToDecorate.toUpperCase()));
+
+    }
+}
---
+++ kodilla-stream/src/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public static void decorate(String stringToDecorate, PoemDecorator poemDecorator) {
+        poemDecorator.decorateText(stringToDecorate);
+    }
+}
