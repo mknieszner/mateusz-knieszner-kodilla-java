57379f64e148b44e5e357c8db1af38208a05834b
mknieszner <mknieszner@gmail.com>
2017-09-02 19:43:42 +0200

    9.1
    Next Ver.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/moviestore/Movie.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/moviestore/Movie.java
@@ -8,20 +8,20 @@ import java.util.List;
  * Movie class.
  */
 public class Movie {
-    private final String id;
-    private final List<String> movieTranslations;
+  private final String id;
+  private final List<String> movieTranslations;
 
 
-    public Movie(final String id, final List<String> movieTranslations) {
-        this.movieTranslations = new ArrayList<>(movieTranslations);
-        this.id = id;
-    }
+  public Movie(final String id, final List<String> movieTranslations) {
+    this.movieTranslations = new ArrayList<>(movieTranslations);
+    this.id = id;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public List<String> getMovieTranslations() {
-        return Collections.unmodifiableList(movieTranslations);
-    }
+  public List<String> getMovieTranslations() {
+    return Collections.unmodifiableList(movieTranslations);
+  }
 }
