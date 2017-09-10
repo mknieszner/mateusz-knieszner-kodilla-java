4c845e1cf91f2f7de5161139b5281a827c070dee
mknieszner <mknieszner@gmail.com>
2017-09-10 23:11:21 +0200

    11.5
    Next Ver.

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/BigMac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/BigMac.java
@@ -15,9 +15,6 @@ public class BigMac {
     private final Map<IngredientCategory, Integer> ingredients = new HashMap<>();
 
     public BigMacBuilder() {
-      addToBigMac(Roll.WITH_SESAME_SEEDS, 1);
-      addToBigMac(Patty.BEEF_PATTY, 2);
-      addToBigMac(Sauce.STANDARD, 1);
     }
 
     public BigMacBuilder ingredients(final IngredientCategory ingredientCategory, final Integer quantity) {
@@ -26,17 +23,44 @@ public class BigMac {
     }
 
     public BigMac build() {
+      addToBigMac(Roll.WITH_SESAME_SEEDS, 1);
+      addToBigMac(Patty.BEEF_PATTY, 2);
+      addToBigMac(Sauce.STANDARD, 1);
       return new BigMac(this);
     }
 
-    private void addToBigMac(final IngredientCategory ingredientCategory, final Integer quantity) {
-      if (ingredients.merge(ingredientCategory, quantity, this::safeMerge) == 0) {
-        ingredients.remove(ingredientCategory);
+    private void addToBigMac(final IngredientCategory ingredientCategory, final Integer quantityToAdd) {
+      if (quantityToAdd > 0) {
+        ingredients.merge(ingredientCategory, quantityToAdd, Integer::sum);
+      } else if (quantityToAdd < 0) {
+        safeMergeIntoIngredients(ingredientCategory, quantityToAdd);
+      } else {
+        throw new IllegalArgumentException("Quantity can not be Zero!");
       }
     }
 
-    private Integer safeMerge(final int existingValue, final int quantity) {
-      return existingValue + quantity >= 0 ? existingValue + quantity : existingValue;
+    private void safeMergeIntoIngredients(final IngredientCategory ingredientCategory, final Integer quantityToAdd) {
+      final Integer presentQuantity = ingredients.get(ingredientCategory);
+      if (presentQuantity != null) {
+        mergeIfSumIsMoreOrEqualZero(presentQuantity, quantityToAdd, ingredientCategory);
+      } else {
+        throw new IllegalArgumentException("Final quantity less than zero!");
+      }
+    }
+
+    private void mergeIfSumIsMoreOrEqualZero(final Integer existingValue, final Integer quantityToAdd, final IngredientCategory ingredientCategory) {
+      final Integer sum = getSumIfMoreThanZero(existingValue, quantityToAdd);
+      if (sum != null) {
+        if (sum == 0) {
+          ingredients.remove(ingredientCategory);
+        } else {
+          ingredients.put(ingredientCategory, quantityToAdd);
+        }
+      }
+    }
+
+    private Integer getSumIfMoreThanZero(final Integer existingValue, final Integer quantityToAdd) {
+      return existingValue + quantityToAdd >= 0 ? existingValue + quantityToAdd : null;
     }
   }
 
--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigMacTestSuite.java
@@ -1,8 +1,11 @@
 package com.kodilla.patterns.builder.bigmac;
 
 import com.kodilla.patterns.builder.pizza.Pizza;
+import com.kodilla.patterns.strategy.social.Millenials;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Test Suite for BigMac class.
@@ -13,20 +16,56 @@ public class BigMacTestSuite {
     //Given
 
     //When
-    final BigMac bigMac = new BigMac.BigMacBuilder()
+    final BigMac bigMac = new BigMac
+        .BigMacBuilder()
         .ingredients(Other.BACON, 1)
         .ingredients(Other.CHILI_PEPPERS, 1)
-        .ingredients(Patty.BEEF_PATTY, -2)
         .ingredients(Patty.CHICKEN_PATTY, 1)
+        .ingredients(Patty.CHICKEN_PATTY, -1)
         .build();
 
     final int ingredientsQuantity = bigMac.getIngredients().values()
         .stream()
         .mapToInt(Integer::intValue)
         .sum();
-    final int expectedIngredientsQuantity = 5;
+    final int expectedIngredientsQuantity = 6;
 
     //Then
     Assert.assertEquals(expectedIngredientsQuantity, ingredientsQuantity);
+  }
+
+  @Rule
+  public ExpectedException expectedEx = ExpectedException.none();
+
+  @Test
+  public void testBigMacBuilderFinalQuantityException() {
+    //Given
+    expectedEx.expect(IllegalArgumentException.class);
+    expectedEx.expectMessage("Final quantity less than zero!");
+
+    //When
+    new BigMac
+        .BigMacBuilder()
+        .ingredients(Patty.BEEF_PATTY, -2)
+        .build();
+
+    //Then
+    //Exception should be thrown
+  }
+
+  @Test
+  public void testBigMacBuilderZeroQuantityException() {
+    //Given
+    expectedEx.expect(IllegalArgumentException.class);
+    expectedEx.expectMessage("Quantity can not be Zero!");
+
+    //When
+    new BigMac
+        .BigMacBuilder()
+        .ingredients(Patty.BEEF_PATTY, 0)
+        .build();
+
+    //Then
+    //Exception should be thrown
   }
 }
